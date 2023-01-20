31cc2148bbffe8938007fee3607ae1fbecdf9a01
wesley viana martins <wesley.vmartins.js@gmail.com>
2023-01-20 14:29:24 -0300

    remove orm doctrine

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,14 +4,25 @@ declare(strict_types=1);
 
 namespace RF\EmployeeManagement\Entity;
 
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+
+#[Entity()]
 class User
 {
+    #[Id, GeneratedValue, Column]
     private int $id;
 
     public function __construct(
+        #[Column]
         public readonly string $firstName,
+        #[Column]
         public readonly string $lastName,
+        #[Column]
         public readonly array $roles,
+        #[Column]
         public readonly string $password,
     ) {  
     }
