1dd367bc19ffdd3a86cc5fe7410815845c953aae
wesley viana martins <wesley.vmartins.js@gmail.com>
2023-01-21 09:15:41 -0300

    create table users

--- create-tables-database.php
+++ create-tables-database.php
@@ -2,7 +2,21 @@
 
 declare(strict_types=1);
 
+$servername="127.0.0.1";
+$username="root";
+$password="-Senha12";
+$dbName="rf_employee_management";
+
 $pdo = new PDO("mysql:host=$servername;dbname=$dbName", $username, $password);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$userTable = "CREATE TABLE users VALUES (id BIGINT AUTO_INCREMENT)";
+$createTableUser = 
+    'CREATE TABLE users (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(100) NOT NULL,
+        roles JSON NOT NULL,
+        password VARCHAR(256) NOT NULL
+    );';
+
+$pdo->exec('write slq or variable with sql');
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,25 +4,14 @@ declare(strict_types=1);
 
 namespace RF\EmployeeManagement\Entity;
 
-use Doctrine\ORM\Mapping\Entity;
-use Doctrine\ORM\Mapping\Column;
-use Doctrine\ORM\Mapping\GeneratedValue;
-use Doctrine\ORM\Mapping\Id;
-
-#[Entity()]
 class User
 {
-    #[Id, GeneratedValue, Column]
     private int $id;
 
     public function __construct(
-        #[Column]
-        public readonly string $firstName,
-        #[Column]
-        public readonly string $lastName,
-        #[Column]
+        public readonly string $name,
+        public readonly string $email,
         public readonly array $roles,
-        #[Column]
         public readonly string $password,
     ) {  
     }
