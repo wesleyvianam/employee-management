cf7cf0968852b1868078479c3d28f05a50b0eadc
wesley viana martins <wesley@dzenvolve.com>
2023-02-16 08:21:13 -0300

    Change Mysql for Sqlite

--- config/dependencyInjection.php
+++ config/dependencyInjection.php
@@ -1,18 +1,10 @@
 <?php
 
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->safeLoad();
-
 $builder = new \DI\ContainerBuilder();
 $builder->addDefinitions([
     PDO::class => function (): PDO {
-        $servername = $_ENV['DB_SERVERNAME'];
-        $username = $_ENV['DB_USERNAME'];
-        $password = $_ENV['DB_PASSWORD'];
-        $dbName = $_ENV['DATABASE_NAME'];
-        $pdo = new PDO("mysql:host=$servername;dbname=$dbName", $username, $password);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $dbPath = __DIR__ . '/../banco.sqlite';
+        $pdo = new PDO("sqlite:$dbPath");
         return $pdo;
     }
 ]);
--- create-tables-database.php
+++ create-tables-database.php
@@ -2,34 +2,16 @@
 
 declare(strict_types=1);
 
-$servername="127.0.0.1";
-$username="root";
-$password="-Senha12";
-$dbName="rf_employee_management";
-
-$pdo = new PDO("mysql:host=$servername;dbname=$dbName", $username, $password);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$dbPath = __DIR__ . '/banco.sqlite';
+$pdo = new PDO("sqlite:$dbPath");
 
 $createTableUser = 
     'CREATE TABLE users (
         id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(100) NOT NULL,
-        email VARCHAR(100) NOT NULL,
-        roles VARCHAR(50) NOT NULL,
-        password VARCHAR(256) NOT NULL
+        name TEXT NOT NULL,
+        email TEXT NOT NULL,
+        roles TEXT NOT NULL,
+        password TEXT NOT NULL
     );';
-
-// $createUserAdmin = 'INSERT INTO users (name, email, roles, password) VALUES (?,?,?,?)';
-
-// // CRIAR USUÀRIO ADMIN
-// $hash_password = password_hash('rf@123', PASSWORD_ARGON2ID);
-// $roles = json_encode(['ROLE_ADMIN']);
-
-// $statement = $pdo->prepare($createUserAdmin);
-// $statement->bindValue(1, "admin");
-// $statement->bindValue(2, "admin@rfemployeemanager.com");
-// $statement->bindValue(3, $roles);
-// $statement->bindValue(4, $hash_password);
-// $statement->execute();
 
 $pdo->exec($createTableUser);
